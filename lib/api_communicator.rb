430c7bc4585bb49e56e8ac0dc42888bd55e40420
github username <github email address>
2019-05-14 15:00:40 +0100

    the team has finished

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -1,11 +1,14 @@
 require 'rest-client'
 require 'json'
-require 'pry'
 
 def get_character_movies_from_api(character_name)
   #make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
+
+  character = find_character(response_hash, character_name)
+
+  get_film_titles(character["films"])
 
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
@@ -20,6 +23,7 @@ end
 
 def print_movies(films)
   # some iteration magic and puts out the movies in a nice list
+  films.each_with_index {|film, i| puts "#{i+1}. #{film}"}
 end
 
 def show_character_movies(character)
@@ -31,3 +35,15 @@ end
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
 # can you split it up into helper methods?
+
+def get_film_titles(films)
+  films.map do |film|
+    JSON.parse(RestClient.get(film))["title"]
+  end
+end
+
+def find_character(data, character_name)
+  data["results"].find do |character|
+    character["name"] == character_name
+  end
+end
